0971f549be19d026ba50a689608843fb3f71b892
Jason <jajones414@gmail.com>
2022-09-13 23:09:00 -0500

    I have situated the index.html and scripts.js files in the workspace.

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ generateBtn.addEventListener("click", writePassword);
 // define password criteria prompts
 // define password length 8-128 characters
 
-// define password character type(uppercase, lowercase, numeric, special)
+// define uppercase, lowercase, numeric, special
 var lowerCaseChars = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
 var upperCaseChars = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
@@ -24,10 +24,9 @@ var upperCaseChars = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L"
 var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
 var specialChars = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~"]
-//
 
 function generatePassword() {
-lengthquestion - confirm("Please select how long you would like your password to be.-Between 8 and 128 digits-");
+lengthquestion - confirm("How long would you like your password to be? -Between 8 and 128 digits-");
 
 lower = confirm("Would you like to use lowercase letters?");
 
@@ -38,6 +37,8 @@ numbers = confirm("Would you like to use numbers?");
 special = confirm("Would you like to use special characters?");
 
 }
+
+
 //var result = "";
 
 //if else
