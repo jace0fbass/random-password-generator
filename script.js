976f106439839bb8981b73a8ff2b06cdfa37df27
Jason <jajones414@gmail.com>
2022-09-17 16:21:35 -0500

    I have commented out most of my previous code in the script.js file and am essentially starting over to understand the relationships better.

--- script.js
+++ script.js
@@ -3,16 +3,56 @@ var generateBtn = document.querySelector("#generate");
 
 generateBtn.addEventListener("click", writePassword);
 
+
 var password = document.getElementById("password");
+var lowerCaseChars = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var upperCaseChars = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var specialChars = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~"];
 
 
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-  
-  passwordText.value = password;
+function generatePassword() {
+var lengthQuestion = parseInt(prompt("How long of a password would you like? Chose between 8-128 digits"));
+  console.log( typeof lengthQuestion);
+  console.log( 'length choice:', lengthQuestion )
+  if (Number.isNaN(lengthQuestion) ) {
+    alert('Please refresh the page and enter a number.')
+    return null;
+  }
+  if (lengthQuestion < 8 || lengthQuestion > 128 ) {
+    alert('Please select a number between 8 and 128.')
+    return null;
+  }
 }
+var lowerQuestion = confirm("Would you like to use lowercase letters?");
+  console.log( 'lower choice', lowerQuestion )
+  
+var upperQuestion = confirm("Would you like to use uppercase letters?");
+  console.log( 'upper choice', upperQuestion )
+  
+var numbersQuestion = confirm("Would you like to use numbers?");
+  console.log( 'numbers choice', numbersQuestion )
+  
+var specialQuestion = confirm("Would you like to use special characters?");
+  console.log( 'special choice', specialQuestion )
 
+
+
+  function writePassword() {
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+    
+    passwordText.value = password;
+  }
+  
+
+
+
+
+
+
+
+  /*
 function userChoices() {
   var lengthQuestion = parseInt(prompt("How long of a password would you like? Chose between 8-128 digits"));
   console.log( typeof lengthQuestion);
