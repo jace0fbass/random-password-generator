cdf2635f1a60461296dfcf051f75b104a77d2d3d
Jason <jajones414@gmail.com>
2022-09-18 16:24:33 -0500

    I have made changes to the spacing and cleaned up the script.js file.

--- assets/script.js
+++ assets/script.js
@@ -12,21 +12,21 @@ function generatePassword() {
 
   // Asks the user to select how long they want their password to be.
   var lengthQuestion = Number(prompt("How long of a password would you like? Choose between 8-128 digits"));
-  console.log( typeof lengthQuestion);
-  console.log( "length choice:", lengthQuestion )
+  console.log(typeof lengthQuestion);
+  console.log("length choice:", lengthQuestion)
 
   // Cycles through the results until the input requirements are fulfilled.
   while (lengthQuestion < 8 || lengthQuestion > 128 || isNaN(lengthQuestion)) {
     if (lengthQuestion < 8 || lengthQuestion > 128 || isNaN(lengthQuestion)) {
 
       // Doesn't allow the user to proceed through the confirms if they don't enter a number type value.
-      if (Number.isNaN(lengthQuestion) ) {
+      if (Number.isNaN(lengthQuestion)) {
         alert("Please enter a number.")
         return null;
       }
 
       // Doesn't allow the user to proceed through the confrims if the number for their password length is less than 8 or more than 128.
-      if (lengthQuestion < 8 || lengthQuestion > 128 ) {
+      if (lengthQuestion < 8 || lengthQuestion > 128) {
         alert("Please select a number between 8 and 128.")
         return null;
       }
@@ -36,16 +36,16 @@ function generatePassword() {
 
   // Asks the user to confirm which character types they would like to use for their password.
   var lowerQuestion = confirm("Would you like to use lowercase letters?");
-  console.log( "lower choice", lowerQuestion )
+  console.log("lower choice", lowerQuestion)
 
   var upperQuestion = confirm("Would you like to use uppercase letters?");
-  console.log( "upper choice", upperQuestion )
+  console.log("upper choice", upperQuestion)
 
   var numbersQuestion = confirm("Would you like to use numbers?");
-  console.log( "numbers choice", numbersQuestion )
+  console.log("numbers choice", numbersQuestion)
 
   var specialQuestion = confirm("Would you like to use special characters?");
-  console.log( "special choice", specialQuestion )
+  console.log("special choice", specialQuestion)
 
   // Forces the user to select at least one character type for their password.
   if (lowerQuestion === false && upperQuestion === false && numbersQuestion === false && specialQuestion === false) {
@@ -65,8 +65,10 @@ function generatePassword() {
 
   for (var i = 0; i <= lengthQuestion; i++) {
   var randomized = userInfo[Math.floor(Math.random() * userInfo.length)]
+  
   // Adds the value of randomized to collectedChars.
   collectedChars += randomized;
+  console.log("collectedChars has been given value");
   };
 
   // Ends function and sends collectedChars to be called out in generatePassword function().
