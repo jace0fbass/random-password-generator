1bf270d39725cec57aec22f00347b0ddb6fae2ee
Jason <jajones414@gmail.com>
2022-09-17 22:02:44 -0500

    I have refactored the script.js file to be much more efficiently viewed and ran..

--- script.js
+++ script.js
@@ -11,16 +11,17 @@ var specialChars = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", 
 function generatePassword() {
   var lengthQuestion = Number(prompt("How long of a password would you like? Choose between 8-128 digits"));
   console.log( typeof lengthQuestion);
-  console.log( 'length choice:', lengthQuestion )
+  console.log( "length choice:", lengthQuestion )
+
   // Cycle through the results until the input is valid.
   while (lengthQuestion < 8 || lengthQuestion> 128 || isNaN(lengthQuestion)) {
     if (lengthQuestion < 8 || lengthQuestion > 128 || isNaN(lengthQuestion)) {
       if (Number.isNaN(lengthQuestion) ) {
-        alert('Please enter a number.')
+        alert("Please enter a number.")
         return null;
       }
       if (lengthQuestion < 8 || lengthQuestion > 128 ) {
-        alert('Please select a number between 8 and 128.')
+        alert("Please select a number between 8 and 128.")
         return null;
       }
       lengthQuestion = Number(prompt("How long of a password would you like? Chose between 8-128 digits"));
@@ -29,40 +30,43 @@ function generatePassword() {
 
   // Asks the user to confirm if they want to use different character types.
   var lowerQuestion = confirm("Would you like to use lowercase letters?");
-  console.log( 'lower choice', lowerQuestion )
+  console.log( "lower choice", lowerQuestion )
 
   var upperQuestion = confirm("Would you like to use uppercase letters?");
-  console.log( 'upper choice', upperQuestion )
+  console.log( "upper choice", upperQuestion )
 
   var numbersQuestion = confirm("Would you like to use numbers?");
-  console.log( 'numbers choice', numbersQuestion )
+  console.log( "numbers choice", numbersQuestion )
 
   var specialQuestion = confirm("Would you like to use special characters?");
-  console.log( 'special choice', specialQuestion )
-  
+  console.log( "special choice", specialQuestion )
+
+  // Forces the user to select at least one character type for their password.
   if (lowerQuestion === false && upperQuestion === false && numbersQuestion === false && specialQuestion === false) {
-    alert('You must chose at least 1 type of character.')
+    alert("You must chose at least 1 type of character.")
     return null;
   }
-
+// Becomes the password.
   var combined = "";
 
   // adds selected characters to an array
-  var userInfo = [].concat(lowerCaseChars);
-  if (upperCaseChars) userInfo = userInfo.concat(upperCaseChars);
-  if (numbers) userInfo = userInfo.concat(numbers);
-  if (specialChars) userInfo = userInfo.concat(specialChars);
+  var userInfo = [].concat(lowerQuestion);
+  if (upperQuestion) userInfo = userInfo.concat(upperCaseChars);
+  if (numbersQuestion) userInfo = userInfo.concat(numbers);
+  if (specialQuestion) userInfo = userInfo.concat(specialChars);
 
   for (var i =1; i <= lengthQuestion; i++) {
   var randomized = userInfo[Math.floor(Math.random() * userInfo.length)]
-  combined +- randomized;
+  combined += randomized;
   };
+
   return combined;
 };
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -70,108 +74,5 @@ function writePassword() {
   passwordText.value = password;
 }
 
-
+// Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-
-
-
-/*if (!userInfo) return null;
-
-if (userInfo.lowerQuestion) {
-  ifChoice = ifChoice.concat(lowerCaseChars)
-  selections.push(getRandom(lowerCaseChars))
-}
-
-if (userInfo.upperQuestion) {
-  ifChoice = ifChoice.concat(upperCaseChars)
-  selections.push(getRandom(upperCaseChars))
-}
-
-if (userInfo.numbersQuestion) {
-  ifChoice = ifChoice.concat(numbers)
-  selections.push(getRandom(numbers))
-}
-
-if (userInfo.specialQuestion) {
-  ifChoice = ifChoice.concat(specialChars)
-  selections.push(getRandom(specialChars))
-}
-
-
-
- 
-
- var userInfo = userChoices();
-  var ifChoice =[];
-  var selections = []; {
-    
-    for (var i = 0; i < ; i++) {
-      upperCaseChars[i] = lowerCaseChars[i].toUpperCase()
-      console.log('Loop there it is');
-    }
-  }
- /* function getRandom(arr) {
-    var randomIndex = Math.floor(Math.random() * arr);
-    var indexElement = arr[randomIndex];
-    return indexElement;
-  }
-  
-  return;
-}
-
-  var lengthQuestion = Number(prompt("How long of a password would you like? Chose between 8-128 digits"));
-  console.log( typeof lengthQuestion);
-  console.log( 'length choice:', lengthQuestion )
-  
-  if (Number.isNaN(lengthQuestion) ) {
-    alert('Please enter a number.')
-    return null;
-  }
-  if (lengthQuestion < 8 || lengthQuestion > 128 ) {
-    alert('Please select a number between 8 and 128.')
-    return null;
-  }
-  
-  var lowerQuestion = confirm("Would you like to use lowercase letters?");
-  console.log( 'lower choice', lowerQuestion )
-  
-  var upperQuestion = confirm("Would you like to use uppercase letters?");
-  console.log( 'upper choice', upperQuestion )
-  
-  var numbersQuestion = confirm("Would you like to use numbers?");
-  console.log( 'numbers choice', numbersQuestion )
-  
-  var specialQuestion = confirm("Would you like to use special characters?");
-  console.log( 'special choice', specialQuestion )
-  
-  if (lowerQuestion === false && upperQuestion === false && numbersQuestion === false && specialQuestion === false) {
-    alert('You must chose at least 1 type of character.')
-    return null;
-  }
-  
-  var userInput = {
-    lengthQuestion: lengthQuestion,
-    lowerQuestion: lowerQuestion,
-    upperQuestion: upperQuestion,
-    numbersQuestion: numbersQuestion,
-    specialQuestion: specialQuestion
-  };
-   
-  return userInput;
-}
-*/
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
-
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-  
-  passwordText.value = password;
-}
-
-
-generateBtn.addEventListener("click", writePassword);
-
-
