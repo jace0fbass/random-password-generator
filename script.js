57b0337e211477261af4a76c2f6104b76c71e565
Jason <jajones414@gmail.com>
2022-09-17 19:35:47 -0500

    I have rearranged the script.js file.

--- script.js
+++ script.js
@@ -1,16 +1,59 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
 
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-  passwordText.value = password;
+var lowerCaseChars = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var upperCaseChars = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var specialChars = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~"];
+
+function generatePassword() {
+  var userInfo = userChoices();
+  var sureChoice = [];
+  var ifChoice =[];
+  var finalChoice = [];
+  var selections = []; {
+    
+    for (var i = 0; i < getRandom.length; i++) {
+      upperCaseChars[i] = lowerCaseChars[i].toUpperCase()
+      console.log('Loop there it is');
+    }
+  }
+  if (!userInfo) return null;
+  
+  if (userInfo.lowerQuestion) {
+    ifChoice = ifChoice.concat(lowerCaseChars)
+    selections.push(getRandom(lowerCaseChars))
+  }
+  
+  if (userInfo.upperQuestion) {
+    ifChoice = ifChoice.concat(upperCaseChars)
+    selections.push(getRandom(upperCaseChars))
+  }
+  
+  if (userInfo.numbersQuestion) {
+    ifChoice = ifChoice.concat(numbers)
+    selections.push(getRandom(numbers))
+  }
+  
+  if (userInfo.specialQuestion) {
+    ifChoice = ifChoice.concat(specialChars)
+    selections.push(getRandom(specialChars))
+  }
+  
+  function getRandom(arr) {
+    var randomIndex = Math.floor(Math.random() * arr);
+    var indexElement = arr[randomIndex];
+    return indexElement;
+  }
+  
+  return;
+  // create more ifs then for loop and run userInput and randomize if choice and push it to final choice. 
+  
+  //for loop on sure choice.length and call out index of surechoice it will equal final choice. 
+  
+  //then return final choice and do a .join
 }
-generateBtn.addEventListener("click", writePassword);
-
 
 function userChoices() {
-  var lengthQuestion = parseInt(prompt("How long of a password would you like? Chose between 8-128 digits"));
+  var lengthQuestion = Number(prompt("How long of a password would you like? Chose between 8-128 digits"));
   console.log( typeof lengthQuestion);
   console.log( 'length choice:', lengthQuestion )
   
@@ -47,69 +90,21 @@ function userChoices() {
     numbersQuestion: numbersQuestion,
     specialQuestion: specialQuestion
   };
-  
- 
-  
+   
   return userInput;
 }
 
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
 
-function generatePassword() {
-  var userInfo = userChoices();
-  var sureChoice = [];
-  var ifChoice =[];
-  var finalChoice = [];
-  var lowerCaseChars = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-  var upperCaseChars = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-  var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-  var specialChars = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~"];
-
-
-      function getRandom(arr) {
-      var randomIndex = Math.floor(Math.random() * arr.length );
-      var indexElement = arr[randomIndex];
-      return indexElement;
-    }
-
-  var selections = []; {
-
-  for (var i = 0; i < getRandom.length; i++) {
-    upperCaseChars[i] = lowerCaseChars[i].toUpperCase()
-    console.log('Loop there it is');
-  }
-}
-  if (!userInfo) return null;
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
   
-  if (userInfo.lowerQuestion) {
-    ifChoice = ifChoice.concat(lowerCaseChars)
-    selections.push(getRandom(lowerCaseChars))
-  }
-  
-  if (userInfo.upperQuestion) {
-    ifChoice = ifChoice.concat(upperCaseChars)
-   selections.push(getRandom(upperCaseChars))
-  }
-  
-  if (userInfo.numbersQuestion) {
-    ifChoice = ifChoice.concat(numbers)
-   selections.push(getRandom(numbers))
-  }
-  
-  if (userInfo.specialQuestion) {
-    ifChoice = ifChoice.concat(specialChars)
-   selections.push(getRandom(specialChars))
-  }
-
-  
-  return;
-  // create more ifs then for loop and run userInput and randomize if choice and push it to final choice. 
-  
-  //for loop on sure choice.length and call out index of surechoice it will equal final choice. 
-  
-  //then return final choice and do a .join
+  passwordText.value = password;
 }
 
 
+generateBtn.addEventListener("click", writePassword);
 
 
-//return result;
